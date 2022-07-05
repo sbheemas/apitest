d46c1838b0edb0bc20bcf673c6eb20901356754d
Sanjay.Bheemasenarao <sanjay.bheemasenarao@gmail.com>
2022-07-05 22:16:50 +0530

    Added Upper API call

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello India"
+
+@app.route("/Upper") 
+def upper():
+    args = request.args
+    input = args.get("input") 
+    return input.upper()
 
 if __name__ == "__main__":
     app.run()
